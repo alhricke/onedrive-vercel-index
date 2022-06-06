d522b873d31e9deeffca31e6d7e664206321e747
alhricke <alhricke@foxmail.com>
2022-06-06 22:57:33 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: [process.env.protectedRoutes],
+  protectedRoutes: ['/软件', '/成品'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:alhricke@foxmail.com',
